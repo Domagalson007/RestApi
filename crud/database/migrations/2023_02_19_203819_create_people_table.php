8163d5fc601bdbb867c46cba2a24f829d9948acf
Domagalson007 <patrykdomagala1111@interia.pl>
2023-02-22 19:30:17 +0100

    restapi

---
+++ crud/database/migrations/2023_02_19_203819_create_people_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('surname', 255);
+            $table->string('phonenum', 255)->Unique();
+            $table->string('street', 255);
+            $table->string('city', 255);
+            $table->string('country', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
--- crud/routes/api.php
+++ crud/routes/api.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PeopleController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
+Route::get('domagala/305356/people', [PeopleController::class, 'index']);
+Route::get('domagala/305356/people/{people}', [PeopleController::class, 'read']);
+Route::delete('domagala/305356/people/{people}', [PeopleController::class, 'delete']);
+Route::post('domagala/305356/people', [PeopleController::class, 'create']);
+Route::put('domagala/305356/people/{people}', [PeopleController::class, 'update']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
