2802839bb23ab04d3272f9c145e9744f14b0e515
Domagalson007 <116101997+Domagalson007@users.noreply.github.com>
2023-02-22 22:30:44 +0100

    Update 2023_02_19_203819_create_people_table.php

--- crud/database/migrations/2023_02_19_203819_create_people_table.php
+++ crud/database/migrations/2023_02_19_203819_create_people_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): 
+    public function up()
     {
         Schema::create('people', function (Blueprint $table) {
             $table->id();
@@ -26,7 +26,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): 
+    public function down()
     {
         Schema::dropIfExists('people');
     }
